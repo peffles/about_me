0b3e2ddcc479c5c2d30af1b4dfbbc17902ed60c2
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-07 12:32:24 -0800

    finished the app

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+var kids = prompt('how many kids do you have?');
+console.log('nimber of kids', kids);
+
+var hats = prompt('how many hats do you own?');
+console.log('number of hats', hats);
+
+if(kids === '4' || kids ==='3'){
+  alert('nice job you right');
+}
+if (hats === '3' && kids === '4'){
+  alert('nice job again!');
+}
+//help!!!!!
+//parseInt
+//toLowerCase
+//toUpperCase
